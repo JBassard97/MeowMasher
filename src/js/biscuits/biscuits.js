3d82c5d74f73f56af6279e3b52c0d00e7b1d72b5
Jbassard <jonathanacciarito@gmail.com>
2025-12-19 23:00:24 -0500

    added comet's arms to biscuits mode, really wanna see how this looks on mobile before commiting to storage stuff

--- src/js/biscuits/biscuits.js
+++ src/js/biscuits/biscuits.js
@@ -1,9 +1,13 @@
 const leftPaw = document.getElementById("left-paw");
 const rightPaw = document.getElementById("right-paw");
+const leftArm = document.querySelector(".left-arm");
+const rightArm = document.querySelector(".right-arm");
 const biscuitsNumber = document.getElementById("biscuits-number");
 
 let numberOfBiscuits = 0;
 biscuitsNumber.textContent = numberOfBiscuits;
+leftArm.style.transform = "translateY(30px)";
+rightArm.style.transform = "translateY(30px)";
 
 let pressIndex = 0;
 let lastPawPressed;
@@ -11,6 +15,8 @@ let lastPawPressed;
 leftPaw.addEventListener("click", (e) => {
   if (leftPaw == lastPawPressed) return;
   lastPawPressed = leftPaw;
+  leftArm.style.transform = "translateY(5px)";
+  rightArm.style.transform = "translateY(30px)";
   pressIndex++;
   console.log("Left", pressIndex);
   if (pressIndex == 2) {
@@ -23,6 +29,8 @@ leftPaw.addEventListener("click", (e) => {
 rightPaw.addEventListener("click", (e) => {
   if (rightPaw == lastPawPressed) return;
   lastPawPressed = rightPaw;
+  rightArm.style.transform = "translateY(5px)";
+  leftArm.style.transform = "translateY(30px)";
   pressIndex++;
   console.log("Right", pressIndex);
   if (pressIndex == 2) {
